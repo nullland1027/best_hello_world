3a4b2fed0ce281abf08a05c835ac67eb856afdd1
Zhanghaohan <nullland1027@outlook.com>
2023-06-27 15:32:15 +0800

    Init project

---
+++ cmake-build-debug/CMakeFiles/best_hello_world.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/best_hello_world.dir/main.cpp.o"
+  "best_hello_world"
+  "best_hello_world.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/best_hello_world.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
